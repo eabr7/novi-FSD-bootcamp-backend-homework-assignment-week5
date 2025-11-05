bc13f302c7f75eb798d7514ad865def1b1483f17
eabr7 <e.bruins@protonmail.com>
2025-11-05 13:15:18 +0100

    Added FirePokemon class

--- src/FirePokemon.java
+++ src/FirePokemon.java
@@ -3,26 +3,36 @@ import java.util.List;
 
 public class FirePokemon extends Pokemon {
     // Variables:
-    String type;
-    List<String> attacks = Arrays.asList(/*voer de aanvallen hier in*/);
+   private final String colorFlame = "red";
+   private final List<String> attacks = Arrays.asList("Fire Lash", "Flame Thrower", "Pyroball", "Inferno");
 
     // Constructor:
     public FirePokemon(String name, int hp, int level, String food, String sound) {
         super(name, hp, level, food, sound);
     }
 
-    // Getters & Setters:
-
-
-    // Methods:
-    List<String> getAttacks() {
-        return attacks;
-
-
+    // Getters:
+    public String getColorFlame() {
+        return colorFlame;
     }
 
-    void fireLash(Pokemon name, Pokemon enemy);
-    void flameThrower(Pokemon name, Pokemon enemy);
-    void pyroBall(Pokemon name, Pokemon enemy);
-    void inferno(Pokemon name, Pokemon enemy);
+    public List<String> getAttacks() {
+        return attacks;
+    }
+
+    void fireLash(Pokemon name, Pokemon enemy){
+        System.out.println(name + "uses Fire Lash on" + enemy);
+    }
+
+    void flameThrower(Pokemon name, Pokemon enemy) {
+        System.out.println(name + "uses Flame Thrower on" + enemy);
+    }
+
+    void pyroBall(Pokemon name, Pokemon enemy) {
+        System.out.println(name + "uses Pyroball on" + enemy);
+    }
+    void inferno(Pokemon name, Pokemon enemy) {
+        System.out.println(name + "uses Inferno on" + enemy);
+    }
 }
+
