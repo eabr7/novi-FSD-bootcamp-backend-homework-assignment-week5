9cc9db6318155e66dcfed12df136d0a32c8974f1
eabr7 <e.bruins@protonmail.com>
2025-11-05 13:12:05 +0100

    Made ElectricPokemon class

--- src/ElectricPokemon.java
+++ src/ElectricPokemon.java
@@ -3,27 +3,38 @@ import java.util.List;
 
 public class ElectricPokemon extends Pokemon {
     // Variables:
-    String type;
-    List<String> attacks = Arrays.asList(/*voer de aanvallen hier in*/);
+    private final String colorElectricity = "white";
+    private final List<String> attacks = Arrays.asList("ThunderPunch", "ElectroBall", "Thunder", "Volt Tackle");
 
     // Constructor:
     public ElectricPokemon(String name, int hp, int level, String food, String sound) {
         super(name, hp, level, food, sound);
     }
 
-    // Getters & Setters:
-
-
-    // Methods:
-    List<String> getAttacks() {
-        return attacks;
-
-
+    // Getters:
+    public String getColorElectricity() {
+        return colorElectricity;
     }
 
-    void thunderPunch(Pokemon name, Pokemon enemy);
-    void electroBall(Pokemon name, Pokemon enemy);
-    void thunder(Pokemon name, Pokemon enemy);
-    void voltTackle(Pokemon name, Pokemon enemy);
+    public List<String> getAttacks() {
+        return attacks;
+    }
 
+// Methods:
+    void thunderPunch(Pokemon name, Pokemon enemy) {
+        System.out.println(name + "uses Thunder Punch on" + enemy);
+    }
+
+    void electroBall(Pokemon name, Pokemon enemy) {
+        System.out.println(name + "uses Electro Ball on" + enemy);
+    }
+
+    void thunder(Pokemon name, Pokemon enemy) {
+        System.out.println(name + "uses Thunder on" + enemy);
+    }
+
+    void voltTackle(Pokemon name, Pokemon enemy) {
+        System.out.println(name + "uses Volt Tackle on" + enemy);
+    }
 }
+
