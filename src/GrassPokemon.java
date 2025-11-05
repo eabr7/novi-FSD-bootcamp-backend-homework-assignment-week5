3c597bde2cd724d94377a5dfcd5aefb2c4fd9670
eabr7 <e.bruins@protonmail.com>
2025-11-05 13:17:59 +0100

    Added GrassPokemon class

--- src/GrassPokemon.java
+++ src/GrassPokemon.java
@@ -3,26 +3,33 @@ import java.util.List;
 
 public class GrassPokemon extends Pokemon {
     // Variables:
-    String type;
-    List<String> attacks = Arrays.asList(/*voer de aanvallen hier in*/);
+    private final int amountLeaves = 50;
+    private final List<String> attacks = Arrays.asList("Solar Beam", "Leaf Storm", "Leech Seed", "Leave Blade");
 
     // Constructor:
     public GrassPokemon(String name, int hp, int level, String food, String sound) {
         super(name, hp, level, food, sound);
     }
 
-    // Getters & Setters:
-
-
-    // Methods:
-    List<String> getAttacks() {
-        return attacks;
-
-
+    // Getters:
+    public int getAmountLeaves() {
+        return amountLeaves;
     }
 
-    public void solarBeam(Pokemon name, Pokemon enemy);
-    public void leafStorm(Pokemon name, Pokemon enemy);
-    public void leechSeed(Pokemon name, Pokemon enemy);
-    public void leaveBlade(Pokemon name, Pokemon enemy);
+    List<String> getAttacks() {
+        return attacks;
+    }
+
+    public void solarBeam(Pokemon name, Pokemon enemy) {
+        System.out.println(name + "uses Solar Beam on" + enemy);
+    }
+    public void leafStorm(Pokemon name, Pokemon enemy) {
+        System.out.println(name + "uses Leaf Storm on" + enemy);
+    }
+    public void leechSeed(Pokemon name, Pokemon enemy) {
+        System.out.println(name + "uses Leech Seed on" + enemy);
+    }
+    public void leaveBlade(Pokemon name, Pokemon enemy) {
+        System.out.println(name + "uses Leave Blade on" + enemy);
+    }
 }
